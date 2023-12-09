fb7b6dd689a016ff1e82515c85fd1975a5d7d833
Anshika Bundela <99082647+LAV1E@users.noreply.github.com>
2023-12-09 20:01:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let i = shortHex.length-3,isCapital = false;
+	let output = "#";
+	while(i<shortHex.length){
+		let asccicode = shortHex[i].charCodeAt(0);
+		if(asccicode >= 65 && asccicode <= 90){
+			isCapital = true;
+		}
+		output += shortHex[i] + shortHex[i];
+	}
+	return  isCapital? output.ToUpperCase:output;
 };
 
 // Do not change the code below.
